7bf4006b822f2704202dde17e5eec1e1fcaaaf5b
menna687 <mennatahha@gmail.com>
2023-04-22 16:09:36 -0700

    Task4: Print dtring using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
-\**
+/**
  * main - print the string "Programming is like building a multilingual puzzle
  * Return: 0 in case of success (no errors)
- *\
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
