7435eb915c45e1a2f5a9b5c3490c551489f46301
menna687 <mennatahha@gmail.com>
2023-04-23 12:02:33 -0700

    Edit coding style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - display the string "with proper grammar, 
- * but the outcome is a piece of art,"
+ * main - display the string (with proper grammar, 
+ * but the outcome is a piece of art,)
  * Return: 0 in case of success (no errors)
  */
 int main(void)
 {
-	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
