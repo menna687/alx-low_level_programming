b4dbd79a497d5ac49a6a26681dbb08ca457b22f8
menna687 <mennatahha@gmail.com>
2023-04-23 11:30:35 -0700

    Task5: print string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - display the string "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0 in case of success (no errors)
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
