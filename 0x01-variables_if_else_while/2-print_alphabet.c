040208753df75fc5eb326e51496862ea138caa51
menna687 <mennatahha@gmail.com>
2023-04-24 17:40:39 -0700

    Task2: Print alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - printthe alphabet in lowercase
+ * Return: 0 in case of success (no errors)
+ */
+int main(void)
+{
+	int i;
+	for(i = 97; i < 123; i++)
+		putchar(i);
+	printf("\n");
+
+	return (0);
+}
