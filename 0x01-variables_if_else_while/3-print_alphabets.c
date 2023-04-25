1aa7c32d6938e88162de6c05dacc0d861ef7c222
menna687 <mennatahha@gmail.com>
2023-04-24 17:49:17 -0700

    Task3: Print alphabets in uppercase and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printthe alphabet in lowercase
+ * Return: 0 in case of success (no errors)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+
+	for (i = 65; i < 91; i++)
+		putchar(i);
+	putchar(10);
+
+	return (0);
+}
