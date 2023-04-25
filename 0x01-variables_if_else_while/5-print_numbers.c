4764a70a4d73260709a8ce4add7352bbcd98acf3
menna687 <mennatahha@gmail.com>
2023-04-24 18:02:27 -0700

    Task5: print digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print digits from 1 to 9
+ * Return: 0 in case of success (no errors)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
