a882165c89268a01c4b33f6388595dd5946116f7
menna687 <mennatahha@gmail.com>
2023-04-25 18:48:58 -0700

    Added task6: function to calculate abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+/**
+ * _abs - compute the absolute value of an integer
+ * @n: number to calculate the absolute value of
+ *
+ * Return: absolute value of the input
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return n;
+}
