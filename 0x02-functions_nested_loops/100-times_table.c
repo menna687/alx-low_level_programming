14ccb65250b5f04126c64579132dde007a70d9f4
menna687 <mennatahha@gmail.com>
2023-04-26 09:36:56 -0700

    Added advanced task 1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: time tables to be printed
+ */
+void print_times_table(int n)
+{
+	if (n > 0 && n <= 15)
+	{
+		int row, col, res;
+
+		for (row = 0; row <= n; row++)
+		{
+			for (col = 0; col <= n; col++)
+			{
+				res = row * col;
+
+				if (res <= 9)
+				{
+					if (col != 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar('0' + res);
+				}
+				else if (res > 9 && res <= 99 )
+				{
+					if (col != 0)
+						_putchar(' ');
+					_putchar('0' + res / 10);
+					_putchar('0' + res % 10);
+				}
+				else
+				{
+					_putchar('0' + res / 100);
+					_putchar('0' + ((res / 10) % 10));
+					_putchar('0' + res % 10);
+				}
+
+				if (col != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,7 @@ int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
-int add(int arg1, int arg2);
 void times_table(void);
+int add(int arg1, int arg2);
 void print_to_98(int n);
+void print_times_table(int n);
