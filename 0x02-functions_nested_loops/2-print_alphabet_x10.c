f55829f4f5f6c1dcb48316a515e7ccd4dd6a9b6d
menna687 <mennatahha@gmail.com>
2023-04-25 17:53:49 -0700

    Task2: Edit coding style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet in lowercase followed by new line 10 times
+ * print_alphabet_x10 - print alphabet in lowercase followed by new line 10 times
  * Return: 0 in case of success (no errors)
  */
 void print_alphabet_x10(void)
 {
 	int i, n;
-	
+
 	for (n = 0; n < 10; n++)
 	{
 		for (i = 97; i < 123; i++)
