e50650c399dc17bc965ed6de610b25b4a064b36e
menna687 <mennatahha@gmail.com>
2023-04-25 17:21:45 -0700

    Added task1: function to print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercase folowed by new line
+ * Return: 0 in case of success (no errors)
+ */
+void print_alphabet (void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
