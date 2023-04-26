4b80cf6f037139a6faffd7e80f4b7cb2ca891846
menna687 <mennatahha@gmail.com>
2023-04-25 19:51:13 -0700

    Added task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - adds two integers
+ * @arg1: first operand
+ * @arg2: second operand
+ *
+ * Return: the sum of the two parameters
+ */
+int add(int arg1, int arg2)
+{
+	return (arg1 + arg2);
+}
