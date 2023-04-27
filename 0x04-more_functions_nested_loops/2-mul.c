0ec49385bb6df816d17f2c0a5d2f2c82475e15e3
menna687 <mennatahha@gmail.com>
2023-04-27 14:03:37 -0700

    Added task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies two integers
+ * @arg1: first operand
+ * @arg2: second operand
+ *
+ * Return: the product of the two parameters
+ */
+int mul(int arg1, int arg2)
+{
+	return (arg1 * arg2);
+}
