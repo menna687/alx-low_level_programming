43925c1c527c2caa06cd886d35a61b791eb5c7ae
menna687 <mennatahha@gmail.com>
2023-04-27 16:57:49 -0700

    Added tasks from 6 to 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_triangle - draws a triangle on the terminal using the character #
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = size; j > i; j--)
+			_putchar(' ');
+		
+		for(k = 0; k < i; k++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
