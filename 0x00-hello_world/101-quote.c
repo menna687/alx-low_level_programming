fe9d551a03e9a52c5fbf667e0f86c32bc9b35dea
menna687 <mennatahha@gmail.com>
2023-04-28 13:16:22 -0700

    Betty coding style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  * and that piece of art is useful " - Dora Korpar, 2015-10-19,
  * followed by a new line, to the standard error
  * without using printf or puts
+ *
+ * Return: Always 1
  */
 int main(void)
 {
