414c909de38d9831e32371d3eaa98e3f2d89e0fd
menna687 <mennatahha@gmail.com>
2023-05-03 18:14:41 -0700

    Update doc

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @*n: pointer to int
+ * @n: pointer to int
  */
 void reset_to_98(int *n)
 {
