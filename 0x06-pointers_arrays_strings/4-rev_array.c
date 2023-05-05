9b4448298ef65505ccd8a3639970031fe5e8b69a
menna687 <mennatahha@gmail.com>
2023-05-05 13:44:51 -0700

    Added task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array of integers
+ * @n: length of array
+ */
+void reverse_array(int *a, int n)
+{
+	int i, tmp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = tmp;
+	}
+}
