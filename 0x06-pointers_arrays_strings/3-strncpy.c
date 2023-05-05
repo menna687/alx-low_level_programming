b122b7506716aeef54b5d2626361f672f3fd6c58
menna687 <mennatahha@gmail.com>
2023-05-05 12:48:22 -0700

    Added task 3

---
+++ 0x06-pointers_arrays_strings/3-strncpy.c
@@ -0,0 +1,26 @@
+/**
+ * strncpy - copies at most  n bytes of the string pointed to by src
+ * to the buffer pointed to by dest
+ * @dest: pointer to buffer address to copy to
+ * @src: pointer to string to be copied
+ * @n : number of bytes to copy from src
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *tmp = dest;
+	int i;
+
+	for (i = 0; ((i < n) && (*src != '\0')); i++)
+	{
+		*tmp = *src;
+		tmp++;
+		src++;
+	}
+	for ( ; i < n; i++)
+	{
+		*tmp = '\0';
+	}
+
+	return (dest);
+}
