4b9b17b393f9fc3869d375179dcf794f698ced5f
menna687 <mennatahha@gmail.com>
2023-05-05 12:16:04 -0700

    Added task 0

---
+++ 0x06-arrays_pointers_strings/0-strcat.c
@@ -0,0 +1,22 @@
+/**
+ * _strcat - concatenates two strings
+ * @dest: string 1 (add string 2 at the end)
+ * @src: string 2
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *tmp = dest;
+
+	while (*tmp != '\0')
+	{
+		tmp++;
+	}
+	while (*src != '\0')
+	{
+		*tmp = *src;
+		tmp++;
+		src++;
+	}
+	return (dest);
+}
