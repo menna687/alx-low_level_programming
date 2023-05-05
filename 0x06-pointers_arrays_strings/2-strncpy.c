9c10e4c0a843fae1ca37ddfdd178ea7f34d68f9a
menna687 <mennatahha@gmail.com>
2023-05-05 12:52:16 -0700

    fix bugs

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,10 @@
 /**
- * strncpy - copies at most  n bytes of the string pointed to by src
+ * strncpy - copies at most n bytes of the string pointed to by src
  * to the buffer pointed to by dest
  * @dest: pointer to buffer address to copy to
  * @src: pointer to string to be copied
  * @n : number of bytes to copy from src
+ *
  * Return: a pointer to the resulting string dest
  */
 char *_strncpy(char *dest, char *src, int n)
@@ -17,10 +18,12 @@ char *_strncpy(char *dest, char *src, int n)
 		tmp++;
 		src++;
 	}
-	for ( ; i < n; i++)
+
+	for (; i < n; i++)
 	{
 		*tmp = '\0';
 	}
+	*tmp = '\0';
 
 	return (dest);
 }
